e9282fe25da09c2a3c426a3e6707a95ffd5bd224
杰克 <1121204968@qq.com>
2021-07-22 19:03:22 +0800

    ios demo完善

---
+++ ios_notepad/ios_notepad/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeView.swift
+//  ios_notepad
+//
+//  Created by orangelab on 2021/7/22.
+//
+
+import UIKit
+
+class HomeView: UIScrollView {
+    let interitemSpacing = 15
+    let lineSpacing = 25
+    var dataArray:Array<String>?
+    var itemArray:Array<UIButton> = Array<UIButton>()
+    func updateLayout(){
+        let itemWidth = (self.frame.size.width-CGFloat(4*interitemSpacing))/3
+        let itemHeight = itemWidth/3*4
+        itemArray.forEach({
+            (element) in element.removeFromSuperview()
+        })
+        itemArray.removeAll()
+        if dataArray != nil && dataArray!.count>0{
+            for index in 0..<dataArray!.count{
+                let btn = UIButton(type: .system)
+                btn.setTitle(dataArray![index], for: .normal)
+                btn.frame = CGRect(
+                    x:CGFloat(interitemSpacing)+CGFloat(index%3)*(itemWidth+CGFloat(interitemSpacing)),
+                    y:CGFloat(interitemSpacing)+CGFloat(index/3)*(itemHeight+CGFloat(lineSpacing)),
+                    width: itemWidth,
+                    height: itemHeight)
+                btn.layer.masksToBounds = true
+                btn.layer.cornerRadius = 15
+                btn.backgroundColor = UIColor(red:1,green:242/255.0,blue:216/255.0,alpha:1)
+                btn.setTitleColor(UIColor.gray, for: .normal)
+                btn.tag = index
+                btn.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
+                self.addSubview(btn)
+                itemArray.append(btn)
+            }
+            self.contentSize = CGSize(width: 0, height: itemArray.last!.frame.origin.y+itemArray.last!.frame.size.height + CGFloat(lineSpacing))
+        }
+    }
+
+   @objc func btnClick(btn:UIButton){
+        print(dataArray![btn.tag])
+    }
+
+}
--- ios_notepad/ios_notepad/ViewController.swift
+++ ios_notepad/ios_notepad/ViewController.swift
@@ -9,18 +9,51 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    lazy var box = UIView()
+    var homeView:HomeView?
+    var dataArray:Array<String>?
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(box)
-               box.backgroundColor = .green
-               box.snp.makeConstraints { (make) -> Void in
-                  make.width.height.equalTo(50)
-                  make.center.equalTo(self.view)
-               }
-        // Do any additional setup after loading the view.
+        self.title = "点滴生活"
+        self.edgesForExtendedLayout = UIRectEdge()
+        dataArray = ["生活","工作","学习","会议列表","健身计划"]
+        self.installUI()
     }
-
-
+    func installUI(){
+        homeView = HomeView(frame: CGRect(x:0,y:0,width: self.view.frame.size.width,height: self.view.frame.size.height-64))
+        self.view.addSubview(homeView!)
+        homeView?.dataArray = ["生活","工作","学习","会议列表","健身计划","生活","工作","学习","会议列表","健身计划","生活","工作","学习","会议列表","健身计划"]
+        homeView?.updateLayout()
+        installNavigationItem()
+    }
+    func installNavigationItem(){
+        let item=UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addGroup))
+        self.navigationItem.rightBarButtonItem=item
+        
+    }
+    @objc func addGroup(){
+        let alertController = UIAlertController(title: "添加记事分组", message:"添加的分组名不能与已有分组重复或为空", preferredStyle: .alert)
+        //swift 闭包知识点
+        alertController.addTextField{(textFiled) in textFiled.placeholder = "请输入记事分组名称"}
+        let alertItem = UIAlertAction(title: "取消", style: .cancel,handler: {(UIAlertAction) in return;})
+        let alertItemAdd = UIAlertAction(title: "确定", style: .default, handler:
+        {(UIAlertAction) -> Void in
+            var exist = false
+            self.dataArray?.forEach({(element) in if element == alertController.textFields?.first!.text || alertController.textFields?.first!.text?.count == 0{
+                exist = true
+            }})
+            if exist{
+                return
+            }
+            self.dataArray?.append(alertController.textFields!.first!.text!)
+            self.homeView?.dataArray = self.dataArray
+            self.homeView?.updateLayout()
+            
+         })
+        alertController.addAction(alertItem)
+        alertController.addAction(alertItemAdd)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
 
